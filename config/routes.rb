ed956b80230d7595644af969cc6d7c58e012aecc
benoit bessiere <benoit.bessiere@gmail.com>
2020-04-01 16:55:06 +0200

    set responsive navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'projects', to: 'pages#projects', as: :projects
+  get 'contact', to: 'pages#contact', as: :contact
 end

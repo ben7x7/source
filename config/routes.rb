fbe202cf0b07a7909fc64038f62629f596f0a9ab
benoit bessiere <benoit.bessiere@gmail.com>
2021-11-03 11:06:07 +0100

    add main footer

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,18 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
   get 'accueil', to: 'pages#accueil', as: :accueil
+  get 'animation', to: 'pages#animation', as: :animation
   get 'cards', to: 'pages#cards', as: :cards
   get 'carousel', to: 'pages#carousel', as: :carousel
   get 'contact', to: 'pages#contact', as: :contact
-  get 'footer', to: 'pages#footer', as: :footer
+  get 'pieddepage', to: 'pages#pieddepage', as: :pieddepage
+  get 'icones', to: 'pages#icones', as: :icones
+  get 'images', to: 'pages#images', as: :images
   get 'matrix', to: 'pages#matrix', as: :matrix
   get 'nav', to: 'pages#nav', as: :nav
   get 'projects', to: 'pages#projects', as: :projects
   get 'p404', to: 'pages#p404', as: :p404
+  get 'ressource', to: 'pages#ressource', as: :ressource
   get 'starwars', to: 'pages#starwars', as: :starwars
 
   resources :reservations, only: [ :new, :create ]
